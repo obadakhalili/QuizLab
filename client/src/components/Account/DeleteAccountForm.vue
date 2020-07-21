8266463e97e0adbc383fbcb22c010d771e7c1ba0
Obada <obada.khalili@outlook.com>
2020-07-21 21:47:37 +0300

    Changed confim message when deleting account

--- client/src/components/Account/DeleteAccountForm.vue
+++ client/src/components/Account/DeleteAccountForm.vue
@@ -14,7 +14,7 @@ export default {
   methods: {
     confirm() {
       this.$store.dispatch("updateModalInfo", {
-        message: "Are you sure you want to proceed with deleting the account?",
+        message: "Please be careful. This is not the usual yada yada, Are you sure you want to proceed with deleting this account?",
         procedure: async () => {
           try {
             await API("/user", "delete");
