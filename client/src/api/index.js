7b2dc31d07b4637553c6c13486d0332cd518428d
Obada <obada.khalili@outlook.com>
2020-08-19 12:21:58 +0300

    Removed unnecessary last argument headers in the API function

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,9 +5,9 @@ import { startLogoutProcess } from "@/helpers";
 const baseRoute =
   "/api" + (process.env.NODE_ENV === "development" ? "" : "/v1");
 
-export default function(url, method, body, headers) {
+export default function(url, method, body) {
   return new Promise((resolve, reject) => {
-    axios[method](baseRoute + url, body, headers)
+    axios[method](baseRoute + url, body)
       .then(resolve)
       .catch(async e => {
         if (e.response.status === 401 || e.response.status === 500) {
